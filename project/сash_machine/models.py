fecea77f15ef53471cf05cb6f175004e231d1d39
typicalstandard <118108373+typicalstandard@users.noreply.github.com>
2025-03-15 17:15:33 +0300

    Delete project/сash_machine directory

--- project/сash_machine/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Item(models.Model):
-    title = models.CharField("Наименование", max_length=255)
-    price = models.DecimalField("Стоимость", max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.title
